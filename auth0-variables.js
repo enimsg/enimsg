34dd532ba05fa9da86e4c8e8cbe2b2457cb39024
EniMSG <suall936@gmail.com>
2019-01-23 22:36:08 +1100

    Delete auth0-variables.js

--- auth0-variables.js
+++
@@ -1,6 +0,0 @@
-var AUTH0_CLIENT_ID='AAxhDQCIyVEsD0fg5AUKlcjZ2W1iSnlv';
-var AUTH0_DOMAIN='quickstart.au.auth0.com';
-var AUTH0_CALLBACK_URL=location.href;
-var GET_MSG_API='https://wt-633c746b5b671107262e31cd5ca39596-0.sandbox.auth0-extend.com/enigmsg-api/getmsg';
-var SEND_MSG_API='https://wt-633c746b5b671107262e31cd5ca39596-0.sandbox.auth0-extend.com/enigmsg-api/sendmsg';
-var GET_KEY_API='https://wt-633c746b5b671107262e31cd5ca39596-0.sandbox.auth0-extend.com/enigmsg-api/getkey';
